45f0069157aba1a4a2a57975306a230ff511a2a2
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-09-12 21:09:10 +0900

    chore: 절대 경로 별칭 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -41,4 +42,8 @@ export default defineConfig({
       },
     }),
   ],
+
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
 });
