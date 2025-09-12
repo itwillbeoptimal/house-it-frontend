a7bb87ba9019c59e586f4ffe0ac82217fba388f7
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-09-12 21:36:49 +0900

    chore: 불필요한 기본 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import appLogo from '../../../../../favicon.svg';
-import PWABadge from './PWABadge.tsx';
-import './App.css';
+import PWABadge from '@/PWABadge';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={appLogo} className="logo" alt="house-it logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>house-it</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <PWABadge />
-    </>
-  );
+  return <PWABadge />;
 }
 
 export default App;
