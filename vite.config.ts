ff43b684cac386d770de04ab5cc9d6d96f1cfa05
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-09-12 21:01:03 +0900

    chore: emotion 설치 및 관련 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,44 @@
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }),
+    VitePWA({
+      registerType: 'prompt',
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'house-it',
-      short_name: 'house-it',
-      description: '생활에서 생기는 모든 질문의 답, 하우스잇',
-      theme_color: '#5AD3F5',
-    },
+      manifest: {
+        name: 'house-it',
+        short_name: 'house-it',
+        description: '생활에서 생기는 모든 질문의 답, 하우스잇',
+        theme_color: '#5AD3F5',
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
-})
+      devOptions: {
+        enabled: false,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    }),
+  ],
+});
