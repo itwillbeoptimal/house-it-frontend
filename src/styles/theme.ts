45e704cc2a2b38c417e3b41937e6a9684b7cc95f
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-09-14 00:48:48 +0900

    chore: 색상 테마 상수 업데이트

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,24 @@
 const theme = {
   COLORS: {
+    MAIN: {
+      PRIMARY: '#5ad3f5',
+      SECONDARY: '#b4e5f3',
+      TERTIARY: '#e4f7fd',
+    },
+    LABEL: {
+      PRIMARY: '#111111',
+      SECONDARY: 'rgba(60, 60, 67, 0.6)',
+      TERTIARY: 'rgba(60, 60, 67, 0.3)',
+      QUATERNARY: 'rgba(60, 60, 67, 0.18)',
+    },
+    GRAY: {
+      1: '#8e8e93',
+      2: '#aeaeb2',
+      3: '#c7c7cc',
+      4: '#d1d1d6',
+      5: '#e5e5ea',
+      6: '#f2f2f7',
+    },
     BACKGROUND: '#f7f9f9',
   },
 } as const;
