2e5ea08d2ebe0c9f6081ba0a95f645c546be7bd3
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-09-22 22:22:18 +0900

    chore: jotai 설치 및 Jotai Provider 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider as JotaiProvider } from 'jotai';
 import { ThemeProvider } from '@emotion/react';
 import theme from '@/styles/theme';
 import GlobalStyle from '@/styles/GlobalStyle';
 import PWABadge from '@/PWABadge';
+import Layout from '@/components/Layout';
+import Magazine from '@/pages/Magazine';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <PWABadge />
-    </ThemeProvider>
+    <JotaiProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="/magazine" element={<Magazine />} />
+            </Route>
+          </Routes>
+        </Router>
+        <PWABadge />
+      </ThemeProvider>
+    </JotaiProvider>
   );
 }
 
