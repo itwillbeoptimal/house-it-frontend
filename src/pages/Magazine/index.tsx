99f6eaea5837e36f7e4341cdce85052d4c216e5f
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-09-22 22:03:40 +0900

    feat: 매거진 페이지 UI 구현

---
+++ src/pages/Magazine/index.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useMemo } from 'react';
+import type { MagazineItem } from '@/pages/Magazine/types/Magazine';
+import * as S from '@/pages/Magazine/Magazine.styles';
+import CategoryTab from '@/components/CategoryTab';
+import MagazineCard from '@/pages/Magazine/components/MagazineCard';
+import { TabWrapper } from '@/pages/Magazine/Magazine.styles';
+
+const Magazine: React.FC = () => {
+  const [selectedCategoryId, setSelectedCategoryId] = useState<number>(1);
+
+  const filteredMagazines = useMemo((): MagazineItem[] => {
+    return [];
+  }, [selectedCategoryId]);
+
+  const handleFilterChange = (categoryId: number) => {
+    setSelectedCategoryId(categoryId);
+  };
+
+  return (
+    <S.Container>
+      <TabWrapper>
+        <CategoryTab activeTab="magazine" onFilterChange={handleFilterChange} />
+      </TabWrapper>
+      <S.ContentArea>
+        {filteredMagazines.length > 0 ? (
+          <S.MagazineGrid>
+            {filteredMagazines.map((magazine) => (
+              <MagazineCard key={magazine.id} {...magazine} />
+            ))}
+          </S.MagazineGrid>
+        ) : (
+          <S.EmptyState>
+            <S.EmptyMessage>해당 카테고리의 매거진이 없습니다.</S.EmptyMessage>
+          </S.EmptyState>
+        )}
+      </S.ContentArea>
+    </S.Container>
+  );
+};
+
+export default Magazine;
