63a21be7648a864af2da60c8ccd4b0f767888258
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-09-22 21:42:14 +0900

    feat: 공통 버튼 컴포넌트 구현

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary';
+  size?: 'small' | 'medium' | 'large';
+  fullWidth?: boolean;
+}
+
+const StyledButton = styled.button<{
+  variant: string;
+  size: string;
+  fullWidth: boolean;
+}>`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 500;
+
+  ${(props) => props.fullWidth && 'width: 100%;'}
+
+  ${(props) => {
+    switch (props.size) {
+      case 'small':
+        return `
+          font-size: 12px;
+          padding: 12px ${props.fullWidth ? '0' : '16px'};
+          border-radius: 12px;
+        `;
+      case 'large':
+        return `
+          font-size: 16px;
+          padding: 18px ${props.fullWidth ? '0' : '32px'};
+          border-radius: 20px;
+        `;
+      default:
+        return `
+          font-size: 14px;
+          padding: 16px ${props.fullWidth ? '0' : '20px'};
+          border-radius: 16px;
+        `;
+    }
+  }}
+
+    ${(props) => {
+    switch (props.variant) {
+      case 'secondary':
+        return `
+          background-color: white;
+          box-shadow: inset 0 0 0 1px ${props.theme.COLORS.GRAY[3]};
+          color: ${props.theme.COLORS.LABEL.SECONDARY};
+        `;
+      default:
+        return `
+          background-color: ${props.theme.COLORS.MAIN.PRIMARY};
+          color: white;
+        `;
+    }
+  }}
+
+    &:disabled {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
+`;
+
+const Button: React.FC<ButtonProps> = ({
+  children,
+  variant = 'primary',
+  size = 'medium',
+  fullWidth = false,
+  type = 'button',
+  disabled = false,
+  ...rest
+}) => {
+  return (
+    <StyledButton
+      type={type}
+      variant={variant}
+      size={size}
+      fullWidth={fullWidth}
+      disabled={disabled}
+      {...rest}
+    >
+      {children}
+    </StyledButton>
+  );
+};
+
+export default Button;
