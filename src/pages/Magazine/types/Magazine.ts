ee70b4747d2dc5860e903d7017e1f73fbcaf7b74
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-09-22 22:01:13 +0900

    feat: 매거진 타입 정의 추가

---
+++ src/pages/Magazine/types/Magazine.ts
@@ -0,0 +1,10 @@
+export interface MagazineItem {
+  id: number;
+  categoryId: number;
+  title: string;
+  subtitle: string;
+  author: string;
+  authorProfileUrl: string;
+  createdAt: string;
+  thumbnailUrl: string;
+}
