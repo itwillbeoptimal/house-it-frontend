87a627f8466ab4eaae6641bb45e95a552f205806
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-09-22 22:11:03 +0900

    chore: 카테고리 상수 파일 추가

---
+++ src/constants/categories.ts
@@ -0,0 +1,69 @@
+import FoodIcon from '@/assets/icons/food.png';
+import CleaningIcon from '@/assets/icons/cleaning.png';
+import RepairIcon from '@/assets/icons/repair.png';
+import FinanceIcon from '@/assets/icons/finance.png';
+import InteriorIcon from '@/assets/icons/interior.png';
+import HealthIcon from '@/assets/icons/health.png';
+import BabyIcon from '@/assets/icons/baby.png';
+import DeviceIcon from '@/assets/icons/device.png';
+
+export const MAGAZINE_CATEGORIES = {
+  1: '라이프스타일',
+  2: '자취생활',
+  3: '식품 · 요리',
+  4: '청소 · 세탁',
+  5: '생활 수리 · DIY',
+  6: '생활 경제 · 계약',
+  7: '이사 · 인테리어',
+  8: '건강 · 웰빙',
+  9: '육아 · 반려동물  · 가족',
+  10: '스마트홈 · 가전',
+  11: '여행 · 주말 나들이',
+};
+
+export const QNA_CATEGORIES = {
+  1: '식품 · 요리',
+  2: '청소 · 세탁',
+  3: '생활 수리 · DIY',
+  4: '생활 경제 · 계약',
+  5: '이사 · 인테리어',
+  6: '건강 · 웰빙',
+  7: '육아 · 반려동물',
+  8: '스마트홈 · 가전',
+  9: '기타',
+};
+
+export const QUIZ_CATEGORIES = {
+  1: {
+    title: '식품 · 요리',
+    iconUrl: FoodIcon,
+  },
+  2: {
+    title: '청소 · 세탁',
+    iconUrl: CleaningIcon,
+  },
+  3: {
+    title: '생활 수리 · DIY',
+    iconUrl: RepairIcon,
+  },
+  4: {
+    title: '생활 경제 · 계약',
+    iconUrl: FinanceIcon,
+  },
+  5: {
+    title: '이사 · 인테리어',
+    iconUrl: InteriorIcon,
+  },
+  6: {
+    title: '건강 · 웰빙',
+    iconUrl: HealthIcon,
+  },
+  7: {
+    title: '육아 · 반려동물',
+    iconUrl: BabyIcon,
+  },
+  8: {
+    title: '스마트홈 · 가전',
+    iconUrl: DeviceIcon,
+  },
+};
