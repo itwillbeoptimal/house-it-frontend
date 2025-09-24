fb1e27a4fe9bc049664cbbe8134ea112816e6cdd
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-09-24 21:26:01 +0900

    style: QnA 페이지 스타일 컴포넌트 구현

---
+++ src/pages/QnA/QnA.styles.ts
@@ -0,0 +1,65 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  width: 100%;
+  height: 100dvh;
+`;
+
+export const TabWrapper = styled.div`
+  position: sticky;
+  top: 72px;
+  padding: 0 20px;
+`;
+export const ContentArea = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: 0 20px 20px 20px;
+`;
+
+export const QnAList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const EmptyState = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const EmptyMessage = styled.p`
+  color: ${({ theme }) => theme.COLORS.LABEL.TERTIARY};
+  font-size: 16px;
+`;
+
+export const ActionBar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 12px 0;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+`;
+
+export const CompleteFilter = styled.button`
+  display: flex;
+  align-items: center;
+  padding: 0 4px;
+  gap: 4px;
+`;
+
+export const MyQnAButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  color: ${(props) => props.theme.COLORS.MAIN.PRIMARY};
+
+  & svg {
+    fill: ${(props) => props.theme.COLORS.MAIN.PRIMARY};
+  }
+`;
