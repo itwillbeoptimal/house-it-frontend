ed3ecd7eca367527ffed939ef0c33277958645a1
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-09-27 21:44:44 +0900

    feat: 퀴즈 배너 컴포넌트 구현

---
+++ src/pages/Quiz/components/Banner/index.tsx
@@ -0,0 +1,20 @@
+import * as S from '@/pages/Quiz/components/Banner/Banner.styles';
+import QuizIcon from '@/assets/icons/quiz.svg?react';
+
+const Banner = () => {
+  return (
+    <S.BannerCard>
+      <S.Content>
+        <S.TextSection>
+          <S.SubTitle>매일 쌓이는 생활 지식</S.SubTitle>
+          <S.Title>오늘의 퀴즈 풀기</S.Title>
+        </S.TextSection>
+      </S.Content>
+      <S.IconWrapper>
+        <QuizIcon />
+      </S.IconWrapper>
+    </S.BannerCard>
+  );
+};
+
+export default Banner;
