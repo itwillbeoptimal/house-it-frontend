f7706f4bde3839d484e0f5ff45202573825c0421
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-10-01 16:41:43 +0900

    style: CommentItem 스타일 컴포넌트 작성

---
+++ src/pages/QnADetail/components/CommentItem/CommentItem.styles.ts
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  gap: 12px;
+  padding: 16px 0;
+
+  &:not(:last-child) {
+    border-bottom: 1px solid ${(props) => props.theme.COLORS.GRAY[1]};
+  }
+`;
+
+export const Content = styled.div`
+  flex: 1;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  margin-bottom: 6px;
+`;
+
+export const CommentText = styled.div`
+  font-size: 14px;
+  line-height: 1.5;
+  word-break: break-word;
+`;
