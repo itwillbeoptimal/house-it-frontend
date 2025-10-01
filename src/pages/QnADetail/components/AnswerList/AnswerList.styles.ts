d68ef57059c5fac89ad275c59fb6f53d2318157a
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-10-01 16:45:51 +0900

    style: AnswerList 스타일 컴포넌트 작성

---
+++ src/pages/QnADetail/components/AnswerList/AnswerList.styles.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const AnswersSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin-top: 16px;
+  gap: 16px;
+`;
+
+export const SectionTitle = styled.h2`
+  margin-left: 4px;
+  font-weight: 600;
+  font-size: 18px;
+`;
