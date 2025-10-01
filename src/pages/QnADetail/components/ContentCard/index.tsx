8dcb9cceff79f0caf77f65304d0b5edf7afb15a1
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-10-01 17:46:33 +0900

    feat: ContentCard 이미지 클릭 시 확대 뷰어 기능 추가
    - 이미지 클릭 시 ImageViewerModal을 통해 원본 이미지를 확대하여 볼 수 있는 기능 추가

--- src/pages/QnADetail/components/ContentCard/index.tsx
+++ src/pages/QnADetail/components/ContentCard/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { createPortal } from 'react-dom';
 import type { QnADetail, Answer } from '@/pages/QnADetail/types';
 import { formatTimeAgo } from '@/utils/dateUtils';
 import * as S from '@/pages/QnADetail/components/ContentCard/ContentCard.styles';
 import * as CommonStyles from '@/pages/QnADetail/components/Common.styles';
 import Profile from '@/pages/QnADetail/components/Profile';
+import ImageViewerModal from '@/pages/QnADetail/components/ImageViewerModal';
 import MenuIcon from '@/assets/icons/menu.svg?react';
 import CommentIcon from '@/assets/icons/comment.svg?react';
 import QuestionIcon from '@/assets/icons/question.svg?react';
@@ -22,64 +24,85 @@ const ContentCard: React.FC<ContentCardProps> = ({
   onCommentsClick,
   onFollowUpClick,
 }) => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
   const isQuestion = type === 'question';
   const isAnswer = type === 'answer';
   const answerData = data as Answer;
   const questionData = data as QnADetail;
 
+  const handleImageClick = (imageUrl: string) => {
+    setSelectedImage(imageUrl);
+  };
+
+  const handleCloseImageViewer = () => {
+    setSelectedImage(null);
+  };
+
   return (
-    <S.Container>
-      <S.Header>
-        <S.AuthorInfo>
-          <Profile
-            profileUrl={data.authorProfileUrl}
-            authorName={data.author}
-          />
-          <S.AuthorMeta>
-            <CommonStyles.AuthorName>
-              {isAnswer && answerData.isAI && (
-                <S.AIBadge>
-                  <SparkleIcon />
-                  AI
-                </S.AIBadge>
-              )}
-              {data.author}
-            </CommonStyles.AuthorName>
-            <CommonStyles.CreatedAt>
-              {formatTimeAgo(data.createdAt)}
-            </CommonStyles.CreatedAt>
-          </S.AuthorMeta>
-        </S.AuthorInfo>
-        <S.MenuButton>
-          <MenuIcon />
-        </S.MenuButton>
-      </S.Header>
-      {isQuestion && <S.Title>{questionData.title}</S.Title>}
-      <S.Content>{data.content}</S.Content>
-      {data.images && data.images.length > 0 && (
-        <S.ImageGallery>
-          {data.images.map((image, index) => (
-            <S.ContentImage
-              key={image}
-              src={image}
-              alt={`${isQuestion ? '질문' : '답변'} 이미지 ${index}`}
+    <>
+      <S.Container>
+        <S.Header>
+          <S.AuthorInfo>
+            <Profile
+              profileUrl={data.authorProfileUrl}
+              authorName={data.author}
             />
-          ))}
-        </S.ImageGallery>
-      )}
-      <S.ActionBar>
-        <S.ActionButton onClick={onCommentsClick}>
-          <CommentIcon />
-          댓글 {data.commentCount}
-        </S.ActionButton>
-        {isAnswer && onFollowUpClick && (
-          <S.ActionButton onClick={onFollowUpClick}>
-            <QuestionIcon />
-            추가 질문 {answerData.followUpQuestionCount}
-          </S.ActionButton>
+            <S.AuthorMeta>
+              <CommonStyles.AuthorName>
+                {isAnswer && answerData.isAI && (
+                  <S.AIBadge>
+                    <SparkleIcon />
+                    AI
+                  </S.AIBadge>
+                )}
+                {data.author}
+              </CommonStyles.AuthorName>
+              <CommonStyles.CreatedAt>
+                {formatTimeAgo(data.createdAt)}
+              </CommonStyles.CreatedAt>
+            </S.AuthorMeta>
+          </S.AuthorInfo>
+          <S.MenuButton>
+            <MenuIcon />
+          </S.MenuButton>
+        </S.Header>
+        {isQuestion && <S.Title>{questionData.title}</S.Title>}
+        <S.Content>{data.content}</S.Content>
+        {data.images && data.images.length > 0 && (
+          <S.ImageGallery>
+            {data.images.map((image, index) => (
+              <S.ContentImage
+                key={image}
+                src={image}
+                alt={`${isQuestion ? '질문' : '답변'} 이미지 ${index}`}
+                onClick={() => handleImageClick(image)}
+              />
+            ))}
+          </S.ImageGallery>
         )}
-      </S.ActionBar>
-    </S.Container>
+        <S.ActionBar>
+          <S.ActionButton onClick={onCommentsClick}>
+            <CommentIcon />
+            댓글 {data.commentCount}
+          </S.ActionButton>
+          {isAnswer && onFollowUpClick && (
+            <S.ActionButton onClick={onFollowUpClick}>
+              <QuestionIcon />
+              추가 질문 {answerData.followUpQuestionCount}
+            </S.ActionButton>
+          )}
+        </S.ActionBar>
+      </S.Container>
+      {selectedImage &&
+        createPortal(
+          <ImageViewerModal
+            imageUrl={selectedImage}
+            onClose={handleCloseImageViewer}
+          />,
+          document.body,
+        )}
+    </>
   );
 };
 
