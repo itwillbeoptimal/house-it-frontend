81100f736e66539d0ee6bbb66af123f89be45510
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-10-13 12:03:20 +0900

    feat: 사용자 정보 조회 query 훅 추가

---
+++ src/hooks/queries/useUserInfoQuery.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import type { UserInfoResponse } from '@/apis/user/types';
+import { fetchUserInfo } from '@/apis/user';
+
+const useUserInfoQuery = () => {
+  const hasToken = !!localStorage.getItem('accessToken');
+
+  return useQuery<UserInfoResponse, AxiosError>({
+    queryKey: ['userInfo'],
+    queryFn: fetchUserInfo,
+    enabled: hasToken,
+    staleTime: 5 * 60 * 1000,
+    retry: false,
+  });
+};
+
+export default useUserInfoQuery;
