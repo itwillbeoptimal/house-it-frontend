03a1a4643dcbed4bf16faa0993e77829bc368395
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-10-20 00:27:00 +0900

    feat: 매거진 API 타입 정의 추가

---
+++ src/apis/magazine/types/index.ts
@@ -0,0 +1,59 @@
+export interface MagazineListItem {
+  magazineId: number;
+  magazineCategoryId: number;
+  magazineTitle: string;
+  magazineSubtitle: string;
+  magazineAuthor: string;
+  authorProfileUrl: string;
+  magazineThumbnailUrl: string;
+  createdAt: string;
+}
+
+export interface MagazineListResponse {
+  magazineListItems: MagazineListItem[];
+  currentPage: number;
+  hasNext: boolean;
+}
+
+export interface MagazineDetailResponse {
+  magazineId: number;
+  magazineCategoryId: number;
+  magazineTitle: string;
+  magazineSubtitle: string;
+  magazineContent: string;
+  magazineAuthor: string;
+  authorProfileUrl: string;
+  magazineThumbnailUrl: string;
+  createdAt: string;
+  isScrap: boolean | null;
+}
+
+export interface MagazineScrapResponse {
+  magazineId: number;
+  isScrapped: boolean;
+}
+
+export interface ScrapBoxItem {
+  magazineId: number;
+  magazineTitle: string;
+  magazineSubtitle: string;
+  magazineThumbnailUrl: string;
+  createdAt: string;
+}
+
+export interface ScrapBoxResponse {
+  scrapBoxItems: ScrapBoxItem[];
+  hasNext: boolean;
+}
+
+export interface RecommendedMagazineItem {
+  magazineId: number;
+  magazineTitle: string;
+  magazineSubtitle: string;
+  magazineThumbnailUrl: string;
+  createdAt: string;
+}
+
+export interface RecommendedMagazineResponse {
+  recommendedMagazineItems: RecommendedMagazineItem[];
+}
