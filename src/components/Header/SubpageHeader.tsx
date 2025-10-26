d67d0354f1dd528edb82dd27d06c6a9d3c25ddf9
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-10-27 07:20:12 +0900

    refactor: 서브페이지 헤더 기능 개선
    - 뒤로가기 동작 개선 및 옵션 추가

--- src/components/Header/SubpageHeader.tsx
+++ src/components/Header/SubpageHeader.tsx
@@ -2,20 +2,28 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAtomValue } from 'jotai';
 import { subpageTitleAtom } from '@/atoms/headerAtom';
+import previousPathAtom from '@/atoms/previousPathAtom';
 import * as S from '@/components/Header/SubpageHeader.styles';
 import BackIcon from '@/assets/icons/back.svg?react';
 
 const SubpageHeader: React.FC = () => {
   const navigate = useNavigate();
   const title = useAtomValue(subpageTitleAtom);
+  const prevPath = useAtomValue(previousPathAtom);
+
+  const handleBack = () => {
+    if (prevPath === '/oauth/callback') {
+      navigate('/');
+    } else if (prevPath === '/qna/post' || prevPath?.includes('edit')) {
+      navigate('/qna');
+    } else {
+      navigate(-1);
+    }
+  };
 
   return (
     <S.Container>
-      <S.BackButton
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
+      <S.BackButton onClick={handleBack}>
         <BackIcon />
       </S.BackButton>
       {title && <S.Title>{title}</S.Title>}
