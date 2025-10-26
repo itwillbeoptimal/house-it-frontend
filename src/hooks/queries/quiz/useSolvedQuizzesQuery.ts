60219ef2279adfd7ca8603cd48d6bec81c4382f2
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-10-27 07:20:21 +0900

    feat: 풀이한 퀴즈 조회 쿼리 훅 추가

--- src/hooks/queries/quiz/useSolvedQuizzesQuery.ts
+++ src/hooks/queries/quiz/useSolvedQuizzesQuery.ts
@@ -1,14 +1,19 @@
-import { useQuery } from '@tanstack/react-query';
-import type { SolvedQuizResponse } from '@/apis/quiz/types';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
+import type { SolvedQuizResponse } from '@/apis/quiz/types';
 import { fetchSolvedQuizzes } from '@/apis/quiz';
 
-const useSolvedQuizzesQuery = (categoryIds: number[], pageNum: number = 0) => {
+const useSolvedQuizzesQuery = (categoryIds: number[]) => {
   const hasToken = !!localStorage.getItem('accessToken');
 
-  return useQuery<SolvedQuizResponse, AxiosError>({
-    queryKey: ['quiz', 'solved', categoryIds, pageNum],
-    queryFn: () => fetchSolvedQuizzes(categoryIds, pageNum),
+  return useInfiniteQuery<SolvedQuizResponse, AxiosError>({
+    queryKey: ['quiz', 'solved', categoryIds],
+    queryFn: ({ pageParam = 1 }) =>
+      fetchSolvedQuizzes(categoryIds, pageParam as number),
+    getNextPageParam: (lastPage) => {
+      return lastPage.hasNext ? lastPage.currentPage + 1 : undefined;
+    },
+    initialPageParam: 1,
     enabled: hasToken && categoryIds.length > 0,
     staleTime: 5 * 60 * 1000,
     retry: false,
