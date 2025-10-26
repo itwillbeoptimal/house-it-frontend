50b465a031ca69e044a62feda3781a8fffe08ec1
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-10-27 07:19:59 +0900

    refactor: App 컴포넌트 단순화
    - 라우팅 로직 분리
    - 컴포넌트 구조 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider as JotaiProvider } from 'jotai';
-import { ThemeProvider } from '@emotion/react';
-import theme from '@/styles/theme';
-import GlobalStyle from '@/styles/GlobalStyle';
-import PWABadge from '@/PWABadge';
-import Layout from '@/components/Layout';
+import ScrollInitializer from '@/components/ScrollInitializer';
 import Modal from '@/components/Modal';
 import BottomSheet from '@/components/BottomSheet';
-import Magazine from '@/pages/Magazine';
+import EventHandler from '@/components/EventHandler';
+import PreviousPathTracker from '@/components/PreviousPathTracker';
+import AppRoutes from '@/routes';
 
 function App() {
   return (
-    <JotaiProvider>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Router>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route path="/magazine" element={<Magazine />} />
-            </Route>
-          </Routes>
-        </Router>
-        <PWABadge />
-        <Modal />
-        <BottomSheet />
-      </ThemeProvider>
-    </JotaiProvider>
+    <>
+      <ScrollInitializer />
+      <PreviousPathTracker />
+      <AppRoutes />
+      <EventHandler />
+      <Modal />
+      <BottomSheet />
+    </>
   );
 }
 
