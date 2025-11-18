de0c119cb0f57105730f4ffa1a2850cf0464e1c3
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-11-19 02:15:58 +0900

    refactor: Jotai Provider 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider as JotaiProvider } from 'jotai';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from '@emotion/react';
@@ -16,14 +15,12 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <JotaiProvider>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Router>
-            <App />
-          </Router>
-        </ThemeProvider>
-      </JotaiProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router>
+          <App />
+        </Router>
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
